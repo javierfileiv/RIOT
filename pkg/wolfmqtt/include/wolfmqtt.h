c2a85db0c5c7e2fbb01589565f0a9970741a8f41
Javier FILEIV <javier.fileiv@gmail.com>
2020-12-23 10:04:54 +0100

    initial commit

---
+++ pkg/wolfmqtt/include/wolfmqtt.h
@@ -0,0 +1,86 @@
+#define WOLFMQTT_USER_SETTINGS
+
+#include "wolfmqtt/mqtt_client.h"
+
+/* MQTT Client state */
+typedef enum _MQTTCtxState {
+    WMQ_BEGIN = 0,
+    WMQ_NET_INIT,
+    WMQ_INIT,
+    WMQ_TCP_CONN,
+    WMQ_MQTT_CONN,
+    WMQ_SUB,
+    WMQ_PUB,
+    WMQ_WAIT_MSG,
+    WMQ_UNSUB,
+    WMQ_DISCONNECT,
+    WMQ_NET_DISCONNECT,
+    WMQ_DONE
+} MQTTCtxState;
+
+/* MQTT Client context */
+/* This is used for the examples as reference */
+/* Use of this structure allow non-blocking context */
+typedef struct _MQTTCtx {
+    MQTTCtxState stat;
+
+    void* app_ctx; /* For storing application specific data */
+
+    /* client and net containers */
+    MqttClient client;
+    MqttNet net;
+
+    /* temp mqtt containers */
+    MqttConnect connect;
+    MqttMessage lwt_msg;
+    MqttSubscribe subscribe;
+    MqttUnsubscribe unsubscribe;
+    MqttTopic topics[1];
+    MqttPublish publish;
+    MqttDisconnect disconnect;
+    MqttPing ping;
+#ifdef WOLFMQTT_SN
+    SN_Publish publishSN;
+#endif
+
+    /* configuration */
+    MqttQoS qos;
+    const char* app_name;
+    const char* host;
+    const char* username;
+    const char* password;
+    const char* topic_name;
+    const char* message;
+    const char* pub_file;
+    const char* client_id;
+    uint8_t *tx_buf, *rx_buf;
+    int return_code;
+    int use_tls;
+    int retain;
+    int enable_lwt;
+#ifdef WOLFMQTT_V5
+    int      max_packet_size;
+#endif
+    word32 cmd_timeout_ms;
+#if defined(WOLFMQTT_NONBLOCK)
+    word32  start_sec; /* used for keep-alive */
+#endif
+    word16 keep_alive_sec;
+    word16 port;
+#ifdef WOLFMQTT_V5
+    word16  topic_alias;
+    word16  topic_alias_max; /* Server property */
+#endif
+    uint8_t    clean_session;
+    uint8_t    test_mode;
+#ifdef WOLFMQTT_V5
+    uint8_t    subId_not_avail; /* Server property */
+    uint8_t    enable_eauth; /* Enhanced authentication */
+#endif
+    unsigned int dynamicTopic:1;
+    unsigned int dynamicClientId:1;
+#ifdef WOLFMQTT_NONBLOCK
+    unsigned int useNonBlockMode:1; /* set to use non-blocking mode.
+        network callbacks can return MQTT_CODE_CONTINUE to indicate "would block" */
+#endif
+} MQTTCtx;
